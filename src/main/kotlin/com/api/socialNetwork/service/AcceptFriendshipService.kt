530063c6bc054d1159500af440b56890376c9483
Gustavo Rolan Soares <78105046+gustavorolan@users.noreply.github.com>
2022-10-05 17:21:27 -0300

    correção @valid

--- src/main/kotlin/com/api/socialNetwork/service/AcceptFriendshipService.kt
+++ src/main/kotlin/com/api/socialNetwork/service/AcceptFriendshipService.kt
@@ -1,8 +1,7 @@
 package com.api.socialNetwork.service
 
 import com.api.socialNetwork.controller.dtos.request.AcceptFriendshipRequest
-import javax.validation.Valid
 
 interface AcceptFriendshipService {
-    fun accept(request: @Valid AcceptFriendshipRequest)
+    fun accept(request: AcceptFriendshipRequest)
 }
