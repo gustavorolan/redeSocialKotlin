869a68ac89b80e6aacbe521543e5d201e8148ed4
Gustavo Rolan Soares <78105046+gustavorolan@users.noreply.github.com>
2022-10-09 14:38:21 -0300

    Adição login controller

---
+++ src/main/kotlin/com/api/socialNetwork/controller/LoginController.kt
@@ -0,0 +1,14 @@
+package com.api.socialNetwork.controller
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/login")
+class LoginController {
+
+    @PostMapping
+    fun login() = ResponseEntity.ok()
+}
